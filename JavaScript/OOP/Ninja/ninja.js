801d69063c8c98e09dfec27c9bd42a1097e883a3
Ibrahim Hamdan <ibra111296@gmail.com>
2023-06-06 15:52:56 +0300

    Updated ninja Assignment to complete a new assignment Super Ninja

--- JavaScript/OOP/Ninja/ninja.js
+++ JavaScript/OOP/Ninja/ninja.js
@@ -1,9 +1,9 @@
 class Ninja{
-    constructor(name,health){
+    constructor(name,health,speed=3,strength=3){
         this.name = name;
         this.health = health;
-        this.speed = 3;
-        this.strength =3;
+        this.speed = speed;
+        this.strength =strength;
 
     }
 
@@ -22,3 +22,18 @@ const ninja1 = new Ninja("Hyabusa",5);
 ninja1.sayName();
 ninja1.drinkSake();
 ninja1.showStats();
+
+class Sensei extends Ninja{
+    constructor(name,health=200,speed=10,strength=10){
+        super(name,health,speed,strength);
+        this.wisdom=10;
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
